faadad487b37bf05269236ac5ce20a41e59b0564
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-11-05 13:07:08 +0100

    random forest: Add wrapper for sklearn.ensemble.RandomForestRegressor

---
+++ Orange/regression/random_forest.py
@@ -0,0 +1,21 @@
+import sklearn.ensemble as skl_ensemble
+
+from Orange.regression import SklLearner, SklModel
+
+__all__ = ["RandomForestLearner"]
+
+
+class RandomForestRegressionModel(SklModel):
+    pass
+
+
+class RandomForestRegressionLearner(SklLearner):
+    __wraps__ = skl_ensemble.RandomForestRegressor
+    __returns__ = RandomForestRegressionModel
+    name = 'random forest regression'
+
+    def __init__(self, n_estimators=10, max_features="auto",
+                 random_state=None, max_depth=3, max_leaf_nodes=5,
+                 preprocessors=None):
+        super().__init__(preprocessors=preprocessors)
+        self.params = vars()
