cb15df0869fcb35bd4ab669b87c0bc0b45c399c2
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-11-23 17:35:51 +0100

    owreport: Remove imports from canvas namespace

--- Orange/widgets/utils/signals.py
+++ Orange/widgets/utils/signals.py
@@ -1,7 +1,7 @@
 import copy
 import itertools
 
-from Orange.canvas.registry.description import InputSignal, OutputSignal
+from orangecanvas.registry.description import InputSignal, OutputSignal
 from Orange.widgets.utils import getmembers
 
 # increasing counter for ensuring the order of Input/Output definitions
@@ -14,7 +14,7 @@ class _Signal:
     @staticmethod
     def get_flags(multiple, default, explicit, dynamic):
         """Compute flags from arguments"""
-        from Orange.canvas.registry.description import \
+        from orangecanvas.registry.description import \
             Single, Multiple, Default, NonDefault, Explicit, Dynamic
         return (Multiple if multiple else Single) | \
                 (Default if default else NonDefault) | \
