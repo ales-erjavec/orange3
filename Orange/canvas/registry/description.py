62b297a9b9bed04e6d50bc07933dfea449357a2b
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-03-19 11:43:04 +0100

    Remove DM library and widgets leaving just the base widget and utils

--- Orange/canvas/registry/description.py
+++
@@ -1,9 +0,0 @@
-# Back compatibility stub module.
-import warnings
-
-from Orange.widgets.widget import InputSignal, OutputSignal
-
-warnings.warn(
-    f"{__name__} is deprecated and will be removed.",
-    DeprecationWarning, stacklevel=2
-)
--- doc/data-mining-library/source/reference/data.value.rst
+++
@@ -1,6 +0,0 @@
-##################
-Values (``value``)
-##################
-
-.. automodule:: Orange.data.variable
-    :members: Value
